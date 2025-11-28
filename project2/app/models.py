ede787097e33ae9711e0d8fe104ab05766e2ad22
benitez <juanbenitezsanabria@gmail.com>
2025-11-27 20:16:58 -0500

    Actualizo model.py (líneas 42-90) cambios Juan Benitez

--- project2/app/models.py
+++ project2/app/models.py
@@ -1,3 +1,154 @@
-from django.db import models
 
-# Create your models here.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#juan benitez
+class usuario (models.Model): 
+    nombre_usuario = models.CharField(max_length=50)
+    rol = models.CharField(max_length=20)
+    
+    def __str__(self):
+        return self.nombre_usuario
+    class Meta:
+        verbose_name = "usuario"
+        verbose_name_plural = "usuarios"
+        db_table = "usuario"
+class supervision(models.Model): 
+    id_empleado = models.ForeignKey('empleado', on_delete=models.CASCADE)
+    fecha = models.DateField()
+    descripcion = models.TextField()
+    
+    def __str__(self):
+        return super().__str__()
+    class Meta:
+        verbose_name = "supervision"
+        verbose_name_plural = "supervisiones"
+        db_table = "supervision"
+class reporte(models.Model):
+    id_supervision = models.ForeignKey('supervision', on_delete=models.CASCADE)
+    titulo = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    fecha = models.DateField()
+    
+    def __str__(self):
+        return super().__str__()
+    class Meta:
+        verbose_name = "reporte"
+        verbose_name_plural = "reportes"
+        db_table = "reporte"
+    
+class mantenimiento(models.Model):
+
+    fecha = models.DateField()
+    tipo = models.CharField(max_length=50)
+    descripcion = models.TextField()
+    
+    def __str__(self):
+        return super().__str__()
+    class Meta:
+        verbose_name = "mantenimiento"
+        verbose_name_plural = "mantenimientos"
+        db_table = "mantenimiento"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
