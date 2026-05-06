fc4101fda62334bfeacb7b02d07a971d99ba9f3f
benitez <juanbenitezsanabria@gmail.com>
2026-05-06 03:21:20 -0500

    la pantalla principal ahora es login

--- project2/config/urls.py
+++ project2/config/urls.py
@@ -18,16 +18,22 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.shortcuts import redirect
+
+def home(request):
+    return redirect('login:login')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('', home),
+
+    path('login/', include(('login.urls', 'login'), namespace='login')),
     path('vistas/', include('app.urls')),
-    path('', include(('login.urls', 'login'), namespace='login')),
     path('usuarios/', include(('usuarios.urls', 'usuarios'), namespace='usuarios')),
     path('ia/', include(('ia.urls', 'ia'), namespace='ia')),
     path('webpush/', include('webpush.urls')),
 ]
 
-# Serve media files during development
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
